8b4c5f77a87e69bd5ea20d8d9be9a19be0ce3389
mayuansdu <mayuanucas@qq.com>
2016-12-21 11:04:23 +0800

    spider paper

--- acm.py
+++ acm.py
@@ -29,17 +29,29 @@ def handle_first_page(url, attrs):
 # 处理二级页面
 def handle_second_page(url, attrs):
     # 获得二级页面
+    soup = get_html_text(url)
     # 优先使用DOI链接
-    raw_links = get_html_text(url).find_all(text=re.compile(r'electronic edition via DOI'))
+    raw_links = soup.find_all(text=re.compile(r'electronic edition via DOI'))
     if len(raw_links) == 0:
         # 没有找到DOI链接，就选择使用通过 @ 找到的链接
         raw_links = get_html_text(url).find_all(text=re.compile(r'electronic edition @'))
-    links = map(lambda tmp: tmp.find_parent('a').get('href'), raw_links)
+    links = map(lambda tmp: tmp.find_parent('a'), raw_links)
+    for raw_url in links:
+        tmp = raw_url.find_parent('li', class_='drop-down')
+        if tmp is not None:
+            temp = tmp.find_next_sibling('li', class_='drop-down')
+            if temp is not None:
+                raw_ris = temp.select_one(
+                    'div[class="body"] > ul:nth-of-type(1) > li:nth-of-type(2) > a'
+                )
+                if raw_ris is not None:
+                    ris = raw_ris.get('href')   # 论文信息RIS文件链接
+        print('论文:', raw_url.get('href'))
+        print('ris:', ris)
+        # handle_third_page(raw_url.get('href'), attrs)
+        # time.sleep(get_random_uniform(begin=5.0, end=20.0))
     if links is None:
         print('处理二级页面，没有找到electronic edition链接')
-    for url in links:
-        handle_third_page(url, attrs)
-        time.sleep(get_random_uniform(begin=5.0, end=20.0))
 
 
 # 处理三级页面
@@ -70,10 +82,23 @@ def handle_third_page(url, attrs):
         print('没有找到此链接的EndNote:%s' % (url))
 
 
+# 下载论文描述的ris格式文件保存到本地
 def download_paper_info(url):
-    raw_page = get_phantomjs_page(url)
-    soup = get_html_str(raw_page)
-    print(soup)
+    filename = re.split(r'/', url)[-1]
+    print('name:', filename, url)
+    data = get_html_text(url).get_text()
+    if data is not None:
+        # 将数据存入本地文件中，方便读取和写入数据库
+        filepath = root_dir + filename
+        with open(filepath, 'w') as f:
+            f.write(data)
+            f.flush()
+        write_to_database(filepath)
+
+
+# 把论文信息写入数据库中
+def write_to_database(filepath):
+    pass
 
 
 # 爬取ACMDL的论文
