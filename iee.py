e4c25fde52648033fcf5838d3c73cdc6db0a0c8a
mayuansdu <mayuanucas@qq.com>
2017-02-10 20:27:28 +0800

    ieee每日更新爬虫

--- iee.py
+++ iee.py
@@ -1,7 +1,6 @@
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
 import re
-import os
 import time
 import traceback
 from common import base_dir, log_dir, ieee_updates_url, get_html_str,\
@@ -25,20 +24,29 @@ def handle_first_page(url):
         return None
     options = page_content.find('select', id='updatesDate')
     if options is not None:
-        update_date = options.find_next('option')['value']  #IEEE内容更新日期 eg:20170206
-        if update_date is None:
-            print('没有得到内容更新日期')
-        elif update_date <= '20170202':
-            print(update_date + '的内容已经更新')
+        update_time = options.find_next('option')['value']  #获得IEEE内容更新日期，例如:20170206
+        # 从数据库中读取上次更新的时间
+        db = get_database_connect()
+        collection = db.update_conf
+        setting_dict = collection.find_one({'name':'setting'})
+        if setting_dict:
+            last_update_time = setting_dict['last_update_time']
         else:
-            print('即将更新' + update_date +'的内容')
+            last_update_time = '20170101'
+            collection.insert(dict({'name':'setting','last_update_time':last_update_time,'this_update_time':last_update_time}))
+        if update_time > last_update_time:  # IEEE内容已更新，且本地尚未采集此次数据
+            collection.update_one({'name':'setting'}, {'$set': {'this_update_time': update_time, 'status': 'unfinished'}})
             ul = page_content.find('ul', class_='Browsing')
             if ul is not None:
                 lis = ul.find_all_next('li', class_='noAbstract')
                 urls = list(map(lambda li: 'http://ieeexplore.ieee.org/xpl/' + li.find_next('a').get('href'), lis))
                 handle_second_page(urls)
-    else:
-        print('没有找到updatesDate')
+        elif update_time <= last_update_time:
+            print(update_time + '的内容已经更新')
+        else:
+            print('没有得到内容更新日期')
+    # 本次采集数据完成，将本次更新日期保存到数据库
+    collection.update_one({'name':'setting'}, {'$set': {'last_update_time': update_time, 'status': 'finished'}})
 
 
 # 处理2级页面
@@ -82,37 +90,39 @@ def handle_second_page(urls):
                                     links.append('http://ieeexplore.ieee.org' + temp.get('href'))
         else:
             print('没有找到分页代码' + url)
-        break   # 先采集一个链接的数目
+        break
         time.sleep(get_random_uniform(begin=60.0, end=300.0))
-    handle_third_page(links)    # 已采集到当前页面上的所有3级页面的链接
+    handle_third_page(links)    # 进一步处理已采集到的当前页面上的所有3级页面的链接
 
 
 def handle_third_page(urls):
     print('链接总数为:' + str(len(urls)))
     for url in urls:
-        print('3级页面:' + url)
         data_dict = dict()
         page_content = get_html_str(get_phantomjs_page(url))
         if page_content is None:
-            print('3级页面无法获取')
-            return None
+            continue
         # 论文URL地址
         data_dict['url'] = url
+        # 论文类型
+        data_dict['category'] = 'conference'
+        # IEEE更新论文日期
+        data_dict['update_time'] = time.strftime('%Y%m%d', time.localtime())
+        # 论文采集时间
+        data_dict['spider_time'] = time.strftime('%Y.%m.%d %H:%M:%S', time.localtime())
         # 采集论文名
         if page_content.title is not None:
-            data_dict['title'] = page_content.title.string
+            data_dict['title'] = page_content.title.string[:-22].strip()
         # 采集论文关键词信息
         ul = page_content.find('ul', class_= 'doc-all-keywords-list')
-        if ul is None:
-            print('无法找到ul')
-            return None
-        spans = ul.find_all_next('span')
-        keywords = list()
-        for span in spans:
-            temp = span.find_next('a', class_='ng-binding')
-            if temp is not None:
-                keywords.append(temp.get_text().strip())
-        data_dict['keywords'] = keywords
+        if ul is not None:
+            spans = ul.find_all_next('span')
+            keywords = list()
+            for span in spans:
+                temp = span.find_next('a', class_='ng-binding')
+                if temp is not None:
+                    keywords.append(temp.get_text().strip())
+            data_dict['keywords'] = keywords
         # 采集论文作者信息
         h2 = page_content.find('h2', text='Authors')
         if h2 is not None:
@@ -147,8 +157,6 @@ def handle_third_page(urls):
                     if div_temp:
                         references.append(div_temp.get_text().strip())
                 data_dict['references'] = references
-        else:
-            print('获取论文references信息失败')
         # 获取论文被引用信息
         page_content = get_html_str(get_phantomjs_page(url + 'citations?anchor=anchor-paper-citations-ieee&ctx=citations'))
         if page_content is not None:
@@ -170,7 +178,20 @@ def handle_third_page(urls):
                     if div_temp:
                         citations.append(div_temp.get_text().strip())
             data_dict['citations'] = citations
-        print(data_dict)
+        write_to_database(data_dict)
+        time.sleep(get_random_uniform(begin=10.0, end=300.0))
+
+
+def write_to_database(data):
+    try:
+        db = get_database_connect()
+        db.update_ieee.insert(data)
+    except Exception as e:
+        print('写入数据库出错！', e)
+        print('当前数据字典为：', data)
+        with open(logfile, 'a+', encoding='utf-8') as f:
+            f.write('update_ieee:' + '写入数据库出错！' + str(e) + '\n')
+
 
 # 采集ieee更新的内容
 def update_ieee(urls):
@@ -197,7 +218,6 @@ def run_iee():
 
 
 if __name__ == '__main__':
-    # print(fun())
-    # run_iee()
+    run_iee()
     # handle_second_page(['http://ieeexplore.ieee.org/xpl/tocresult.jsp?isnumber=7842910',])
-    handle_third_page(['http://ieeexplore.ieee.org/document/7140733/',])
+    # handle_third_page(['http://ieeexplore.ieee.org/document/7140733/',])
